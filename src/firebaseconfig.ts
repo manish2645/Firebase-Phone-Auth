6f82b226069a3b931e4af3fe9289f0665cef139e
Manish Kumar Singh <manish@tellisim.com>
2025-02-14 17:59:37 +0530

    remove sensitive Firebase configuration details

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -6,13 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDZgdCpEAkVk6bNoObdStXFot-SAB02AEs",
-    authDomain: "travelesimconnect.firebaseapp.com",
-    projectId: "travelesimconnect",
-    storageBucket: "travelesimconnect.appspot.com",
-    messagingSenderId: "481664715976",
-    appId: "1:481664715976:web:90eddb049de9dde4dde9fd",
-    measurementId: "G-P2ZXZ2HTSZ"
+
   };
 
 // Initialize Firebase
